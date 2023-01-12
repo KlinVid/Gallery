8b00f9216829d18ef2e27e9e23ff49c870be33c7
KlinVid <night.stylker@gmail.com>
2023-01-12 17:58:30 +0300

    ver2

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,7 @@ for (i=0; i<galleryItem.length; i++){
     
         <div class="gallery__item">
             <div class="img__gallery">
-                <img id='${galleryItem[i].id}' class="picture" src="assets/images/${galleryItem[i].pic}" alt="">
+                <img onclick="showImg('${galleryItem[i].pic}','${galleryItem[i].text}')" id='${galleryItem[i].id}' class="picture" src="assets/images/${galleryItem[i].pic}" alt="">
             </div>
             <div class="title__gallery">
                 ${galleryItem[i].title}
@@ -44,90 +44,16 @@ for (i=0; i<galleryItem.length; i++){
 
 
 
-// выполнение события onclick при нажатии на одну их трех картинок 
-document.getElementById('id1').addEventListener('click', pic1);
-document.getElementById('id2').addEventListener('click', pic2);
-document.getElementById('id3').addEventListener('click', pic3);
-let zoom = '';
-      
-//добавление ниже увеличенной картинки mir.jpg с тесктом
-function pic1() {
-    zoom = `
-            <div class="zoom__item">
-                <div class="zoom__img">
-                    <img class="zoom__pic" src="assets/images/${galleryItem[0].pic}" alt="">
-                </div>
-                <div class="zoom__text">
-                    ${galleryItem[0].text}
-                </div>
+function showImg(p,t) {
+    let zoom  = `
+        <div class="zoom__item" id='bigImg'>
+            <div class="zoom__img">
+                <img onclick="bigImg.innerHTML =''" src="assets/images/${p}" alt="" style="width: 1000px;">
             </div>
-            `;
-
-    document.querySelector('.zoom__inner').innerHTML = zoom;
-
-    // удаление увеличенной картинки, если на неё нажать
-    let del = document.querySelector('.zoom__pic');
-    if (del){
-        del.addEventListener('click', delPic);
-        function delPic(){
-            zoom = '';
-            document.querySelector('.zoom__inner').innerHTML = zoom;
-        };
-    }
-};
-
-//добавление ниже увеличенной картинки red.jpg с тесктом
-function pic2() {
-    zoom = `
-            <div class="zoom__item">
-                <div class="zoom__img">
-                    <img class="zoom__pic" src="assets/images/${galleryItem[1].pic}" alt="">
-                </div>
-                <div class="zoom__text">
-                    ${galleryItem[1].text}
-                </div>
+            <div class="zoom__text">
+                ${t}    
             </div>
-            `;
-
+        </div>
+    `;
     document.querySelector('.zoom__inner').innerHTML = zoom;
-
-    // удаление увеличенной картинки, если на неё нажать
-    let del = document.querySelector('.zoom__pic');
-    if (del){
-        del.addEventListener('click', delPic);
-        function delPic(){
-            zoom = '';
-            document.querySelector('.zoom__inner').innerHTML = zoom;
-        };
-    }
-};
-
-//добавление ниже увеличенной картинки troi.jpg с тесктом
-function pic3() {
-    zoom = `
-            <div class="zoom__item">
-                <div class="zoom__img">
-                    <img class="zoom__pic" src="assets/images/${galleryItem[2].pic}" alt="">
-                </div>
-                <div class="zoom__text">
-                    ${galleryItem[2].text}
-                </div>
-            </div>
-            `;  
-
-    document.querySelector('.zoom__inner').innerHTML = zoom;
-
-    // удаление увеличенной картинки, если на неё нажать
-    let del = document.querySelector('.zoom__pic');
-    if (del){
-        del.addEventListener('click', delPic);
-        function delPic(){
-            zoom = '';
-            document.querySelector('.zoom__inner').innerHTML = zoom;
-        };
-    }
-
-};
-
-
-
+}
